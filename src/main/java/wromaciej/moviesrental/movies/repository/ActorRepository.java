7b4957f65a0068ab8524a863ca9978c76a54b8f9
Maciej Wróblewski <wro.maciej@gmail.com>
2018-02-10 15:57:40 +0100

    Issue #6 solved by HQL, thanks for rejecting last commit!

--- src/main/java/wromaciej/moviesrental/movies/repository/ActorRepository.java
+++ src/main/java/wromaciej/moviesrental/movies/repository/ActorRepository.java
@@ -1,6 +1,8 @@
 package wromaciej.moviesrental.movies.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import wromaciej.moviesrental.movies.model.Actor;
 
@@ -9,6 +11,9 @@ import java.util.List;
 @Repository
 public interface ActorRepository extends CrudRepository<Actor, Integer> {
     List<Actor> findByFirstName(String firstName);
-    List<Actor> findByLastName(String lastName);
+    //List<Actor> findByLastName(String lastName);
     List<Actor> findByFirstNameAndLastName(String firstName, String lastName);
+
+    @Query("SELECT actor FROM Actor actor WHERE LOWER(actor.lastName) = LOWER(:name)")
+    List<Actor> findActorsWithLastName(@Param("name") String name);
 }
