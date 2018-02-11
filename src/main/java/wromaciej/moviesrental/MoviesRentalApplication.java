dc8720d2f70517e608455d46b97e916b5ac96900
Maciej Wróblewski <wro.maciej@gmail.com>
2018-02-11 21:56:36 +0100

    UssueNo7 DONE - everything works fine

--- src/main/java/wromaciej/moviesrental/MoviesRentalApplication.java
+++ src/main/java/wromaciej/moviesrental/MoviesRentalApplication.java
@@ -2,13 +2,10 @@ package wromaciej.moviesrental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @SpringBootApplication
 public class MoviesRentalApplication  {
-
 
 	public static void main(String[] args) {
 
